f4981397330d1f354a1384c3cb0fc7cbfeac9528
Rashad Raul <rashad.tgr@gmail.com>
2024-03-04 04:08:45 +0400

    Complete task 4, 5 : login tests

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## AutoQA with Java
 
-- [Lesson 1 : testcases.xlsx](./src/test/resources)
+[xml resources : for branch `lssn#03`](https://github.com/Raul-ALab/aqa-kit-java/blob/lssn%2303/src/test/resources)
+
+- [Lesson_3](https://github.com/Raul-ALab/aqa-kit-java/tree/lssn%2303/src/test/java/org/raul/lesson_3)
+- [Lesson_2](https://github.com/Raul-ALab/aqa-kit-java/tree/lssn%2302/src/test/java/org/raul/lesson_2)
+- [Lesson_1 : testcases.xlsx](./src/test/resources)
--- src/test/java/org/raul/lesson_3/loginsuccess/LoginParameters.java
+++ src/test/java/org/raul/lesson_3/loginsuccess/LoginParameters.java
@@ -1,24 +1,25 @@
-package org.raul.lesson_3.dataprovider;
+package org.raul.lesson_3.loginsuccess;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.raul.lesson_3.utils.DriverSetUp;
-import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 /*
- * Task 4: Заранее создать трех пользователей для нашего сайта. Написать тест
- * используя @DataProvider который будет проверять логин этих трех
- * пользователей.
+ * Task 5: Решить предыдущую задачу используя аннотацию @Parameters.
+ * А также создать для работы с данным тестом дополнительный xml файл
+ * testngParametersHome.xml.
  * */
-public class LoginDataProvider {
+public class LoginParameters {
 
-    @Test(dataProvider = "credentials")
-    public void loginSuccessTest(String email, String password) {
+    @Test
+    @Parameters({"email", "password"})
+    public void loginSuccessTestParameters(String email, String password) {
         WebDriver driver = DriverSetUp.setUpDriver();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
         driver.get("https://qa-course-01.andersenlab.com/login");
@@ -31,15 +32,5 @@ public class LoginDataProvider {
         wait.until(ExpectedConditions.elementToBeClickable(logoutLocator));
 
         driver.quit();
-    }
-
-
-    @DataProvider(name = "credentials")
-    public Object[][] loginData() {
-        return new Object[][]{
-                {"mboone@example.com", "boone123"},
-                {"spader@example.com", "js123456"},
-                {"laura@example.com", "ls123456"}
-        };
     }
 }
