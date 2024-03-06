e9213ad106f1683570402c5e8fe99fe085b20109
Rashad Raul <rashad.tgr@gmail.com>
2024-03-06 12:56:41 +0400

    Refactor driver set up with WebDriverManager

---
+++ src/main/java/org/raul/main/App.java
@@ -0,0 +1,7 @@
+package org.raul.main;
+
+public class App {
+    public static void main( String[] args ) {
+
+    }
+}
---
+++ src/main/java/org/raul/utils/DriverSetUp.java
@@ -0,0 +1,20 @@
+package org.raul.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverSetUp {
+    public static WebDriver setUpDriver() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+        return driver;
+    }
+}
