5d713e2214a49ce4e2a274d323cd41b2a2ac2e29
Rashad Raul <rashad.tgr@gmail.com>
2024-03-14 02:42:53 +0400

    Testcase: L03

---
+++ src/main/java/org/raul/lesson_5/testcases/UsernameDuplication.java
@@ -0,0 +1,73 @@
+package org.raul.lesson_5.testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+/*
+ * Testcase ID: L03
+ * 2. Доделать по три автотеста из каждого модуля, на которые писали тест-кейсы в лекции 9.
+ * */
+public class UsernameDuplication {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    private By headerLocator = By.xpath("//h1[@class='text-2xl']");
+    private By fNameLocator = By.xpath("//input[@name='firstName']");
+    private By lNameLocator = By.xpath("//input[@name='lastName']");
+    private By birthdayLocator = By.xpath("//input[@name='dateOfBirth']");
+    private By emailLocator = By.xpath("//input[@name='email']");
+    private By passwordLocator = By.xpath("//input[@name='password']");
+    private By confirmPassLocator = By.xpath("//input[@name='passwordConfirmation']");
+    private By submitLocator = By.xpath("//button[@type='submit']");
+    private By editLocator = By.xpath("//div//a[contains(text(), 'Edit')]/following-sibling::p[text()='Logout']");
+
+    public UsernameDuplication(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(1));
+    }
+
+    public String signUpHeader() {
+        return driver.findElement(headerLocator).getText();
+    }
+
+    public void inputName(String fName) {
+        driver.findElement(fNameLocator).sendKeys(fName);
+    }
+
+    public void inputLastName(String lName) {
+        driver.findElement(lNameLocator).sendKeys(lName);
+    }
+
+    public void inputBirthday(String birthdate) {
+        driver.findElement(birthdayLocator).sendKeys(birthdate);
+        driver.findElement(emailLocator).click(); // to dismiss calendar
+    }
+
+    public void inputEmail(String email) {
+        driver.findElement(emailLocator).sendKeys(email);
+    }
+
+    public void inputPassword(String password) {
+        driver.findElement(passwordLocator).sendKeys(password);
+    }
+
+    public void inputConfirmPassword(String confirmPassword) {
+        driver.findElement(confirmPassLocator).sendKeys(confirmPassword);
+        driver.findElement(submitLocator).click();
+    }
+
+    public boolean isDuplicateAllowed() {
+        String registrationUrl = "registration";
+        try {
+            wait.until(ExpectedConditions.not(ExpectedConditions.urlContains(registrationUrl)));
+        } catch (TimeoutException e) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/org/raul/lesson_5/testcases/UsernameDuplicationTest.java
@@ -0,0 +1,51 @@
+package org.raul.lesson_5.testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.raul.utils.DriverSetUp;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+/*
+ * Testcase ID: L03
+ * 2. Доделать по три автотеста из каждого модуля, на которые писали тест-кейсы в лекции 9.
+ * */
+public class UsernameDuplicationTest {
+    private final static String URL = "https://qa-course-01.andersenlab.com/registration";
+
+    private WebDriver driver;
+    private UsernameDuplication duplication;
+
+    @BeforeClass
+    public void setUp() {
+        driver = DriverSetUp.setUpChromeDriver();
+        duplication = new UsernameDuplication(driver);
+        driver.get(URL);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test(dataProvider = "registration")
+    public void verifyRegistrationPageInputFields(String fName, String lName, String birthdate,
+                                                  String email, String password, String confirmPassword) {
+        duplication.inputName(fName);
+        duplication.inputLastName(lName);
+        duplication.inputBirthday(birthdate);
+        duplication.inputEmail(email);
+        duplication.inputPassword(password);
+        duplication.inputConfirmPassword(confirmPassword);
+
+        Assert.assertFalse(duplication.isDuplicateAllowed(), "Duplicate username shouldn't be allowed!");
+    }
+
+    @DataProvider(name = "registration")
+    public Object[][] registrationData() {
+        return new Object[][]{
+                {"Elizabeth", "Keen", "03/23/2000", "mboone15@example.com", "mbmb.123456", "mbmb.123456"}
+        };
+    }
+}
