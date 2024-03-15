15f4356c8ca276c225495bcb8c4b8b63cbe6e401
Rashad Raul <rashad.tgr@gmail.com>
2024-03-16 02:15:26 +0400

    Copy missing lssn5 from branch #05:main

---
+++ src/main/java/org/raul/lesson_5/testcases/LoginSession.java
@@ -0,0 +1,50 @@
+package org.raul.lesson_5.testcases;
+
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+/* Testcase ID: L019 */
+public class LoginSession extends LoginProcessor {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public LoginSession(WebDriver driver) {
+        super(driver);
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+    }
+
+    public boolean compareUsers() {
+        String initialWindowUser = getUsername();
+        String secondWindowUser = "";
+
+        boolean isSwitchSuccessful = copyUrlToNewWindow();
+        if (isSwitchSuccessful) {
+            secondWindowUser = getUsername();
+        } else {
+            return false;
+        }
+        return initialWindowUser.equals(secondWindowUser);
+    }
+
+    public boolean copyUrlToNewWindow() {
+        String currentUrl = driver.getCurrentUrl();
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.get(currentUrl);
+        try {
+            wait.until(ExpectedConditions.visibilityOf(loggedInEmailElement)).isDisplayed();
+        } catch (TimeoutException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private String getUsername() {
+        return loggedInEmailElement.getText();
+    }
+}
