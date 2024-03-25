26b8be3376c479e0d71e3c79f3e70c0057872ca2
Rashad Raul <rashad.tgr@gmail.com>
2024-03-25 20:28:01 +0400

    Add missing steps for Allure report

--- src/main/java/org/raul/lesson_7/ErrorMessageObscurity.java
+++ src/main/java/org/raul/lesson_7/ErrorMessageObscurity.java
@@ -1,5 +1,6 @@
 package org.raul.lesson_7;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -19,42 +20,49 @@ public class ErrorMessageObscurity implements AqaPageLocators {
         this.driver = driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
-
+    @Step("Enter invalid email: {invalidEmail}")
     public ErrorMessageObscurity inputInvalidEmail(String invalidEmail) {
         driver.findElement(EMAIL_LOCATOR).sendKeys(invalidEmail);
         return this;
     }
 
+    @Step("Enter valid password: {validPassword}")
     public ErrorMessageObscurity inputPassword(String validPassword) {
         driver.findElement(PASSWORD_LOCATOR).sendKeys(validPassword);
         return this;
     }
 
+    @Step("Enter valid email: {validEmail}")
     public ErrorMessageObscurity inputEmail(String validEmail) {
         driver.findElement(EMAIL_LOCATOR).sendKeys(validEmail);
         return this;
     }
 
+    @Step("Enter invalid password: {invalidPassword}")
     public ErrorMessageObscurity inputInvalidPassword(String invalidPassword) {
         driver.findElement(PASSWORD_LOCATOR).sendKeys(invalidPassword);
         return this;
     }
 
+    @Step("Click sign in")
     public ErrorMessageObscurity clickSignInBtn() {
         wait.until(ExpectedConditions.elementToBeClickable(SIGN_IN_LOCATOR)).click();
         return this;
     }
 
+    @Step("Switch to another Tab")
     public ErrorMessageObscurity switchToAnotherTab() {
         driver.switchTo().newWindow(WindowType.TAB);
         return this;
     }
 
+    @Step("Reopen login page")
     public ErrorMessageObscurity openLoginPage(String url) {
         driver.get(url);
         return this;
     }
 
+    @Step("Get error message for comparison")
     public String getErrorMessageForInvalidCredentials() {
         try {
             WebElement errorMessageElement = wait.until(ExpectedConditions.visibilityOfElementLocated(ERROR_LOCATOR));
--- src/main/java/org/raul/lesson_7/LoginRedirectAfterLogout.java
+++ src/main/java/org/raul/lesson_7/LoginRedirectAfterLogout.java
@@ -1,5 +1,6 @@
 package org.raul.lesson_7;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -19,46 +20,55 @@ public class LoginRedirectAfterLogout implements AqaPageLocators {
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
+    @Step("Enter email: {email}")
     public LoginRedirectAfterLogout inputEmail(String email) {
         driver.findElement(EMAIL_LOCATOR).sendKeys(email);
         return this;
     }
 
+    @Step("Enter password: {password}")
     public LoginRedirectAfterLogout inputPassword(String password) {
         driver.findElement(PASSWORD_LOCATOR).sendKeys(password);
         return this;
     }
 
+    @Step("Click Sign in button")
     public LoginRedirectAfterLogout clickSignInBtn() {
         wait.until(ExpectedConditions.elementToBeClickable(SIGN_IN_LOCATOR)).click();
         return this;
     }
 
+    @Step("Check Logout button")
     public boolean isLoggedIn() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(LOGOUT_LOCATOR));
         return driver.findElement(LOGOUT_LOCATOR).isDisplayed();
     }
 
+    @Step("Click Logout button")
     public LoginRedirectAfterLogout clickLogout() {
         wait.until(ExpectedConditions.elementToBeClickable(LOGOUT_LOCATOR)).click();
         return this;
     }
 
+    @Step("Confirm logout")
     public LoginRedirectAfterLogout confirmYesToLogout() {
         wait.until(ExpectedConditions.elementToBeClickable(CONFIRM_YES_LOCATOR)).click();
         return this;
     }
 
+    @Step("Check if logged out")
     public boolean isLoggedOut() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(SIGN_IN_LOCATOR));
         return driver.findElement(SIGN_IN_LOCATOR).isDisplayed();
     }
 
+    @Step("Click back button")
     public LoginRedirectAfterLogout backButtonRedirect() {
         driver.navigate().back();
         return this;
     }
 
+    @Step("Check page state after clicking on the back button")
     public boolean isBackToLogInPage() {
         return driver.findElements(LOGOUT_LOCATOR).isEmpty() ? false : true;
     }
